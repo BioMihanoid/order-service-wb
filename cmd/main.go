6717355a1cf158f6f042f038c2d4b90ab5f5fb75
Pechnikov Mikhail <pechnikovmichelle@gmail.com>
2025-07-06 02:35:53 +0300

    add base config

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"order-service-wb/pkg/config"
+)
 
+func main() {
+	conf := config.NewConfig()
+	fmt.Printf("%+v\n", conf)
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DbConfig DbConfig `mapstructure:"db"`
+}
+
+type DbConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	Database string `mapstructure:"db_name"`
+	SSLMode  string `mapstructure:"ssl_mode"`
+}
+
+func NewConfig() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	configPath := os.Getenv("CONFIG_PATH")
+	configName := os.Getenv("CONFIG_NAME")
+	if configPath == "" || configName == "" {
+		log.Fatal("CONFIG_PATH or CONFIG_NAME environment variable not set")
+	}
+
+	viper.SetConfigName(configName)
+	viper.AddConfigPath(configPath)
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("Error reading config file")
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Fatal("Error unmarshalling config")
+	}
+
+	return &config
+}
